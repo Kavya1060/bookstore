93ea5ff4d865fc7ad64379b7c9cc243bc3abdb77
Kavya1060 <kavyasaxena108@gmail.com>
2025-08-02 03:16:56 +0530

    Delete ApiError.js

--- ApiError.js
+++
@@ -1,9 +0,0 @@
-class ApiError {
-  constructor(statusCode, message = "Something went wrong", errors = []) {
-    this.statusCode = statusCode;
-    this.message = message;
-    this.errors = errors;
-  }
-}
-
-export { ApiError };
